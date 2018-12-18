ee31d209d885682c3ab018d2be7f6d2478710ee7
ramyasutraye <43085084+ramyasutraye@users.noreply.github.com>
2018-12-18 12:29:30 +0530

    Update N elements.py

--- N elements.py
+++ N elements.py
@@ -1,10 +1,5 @@
-try:
- z1=str(input())
- y1=z1.split(" ")
- v=int(y1[1])
- z=str(input())
- y=z.split(" ")
- y.sort(reverse=True)
- print(y[v-1])
-except:
- print("Invalid")
+z,v=map(int,input().split())
+y=input().split()
+x=len(y)
+y.sort()
+print(y[x-v])
